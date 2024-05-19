27a8e5c00bb65306322c61cb6c8b0074a3689152
VijayMeena701 <govardhanmeena701@gmail.com>
2024-05-19 19:29:31 +0530

    node-2

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ server.use(express.static(path.resolve(__dirname,process.env.PUBLIC_DIR)));
 server.use("/products", productRouter.router);
 server.use("/users", userRouter.router);
 server.use('*',(req,res)=>{
-  res.sendFile(path.resolve(__dirname+'build','index.html'))
+  res.sendFile(path.resolve(__dirname,'build','index.html'))
 })
 
 
